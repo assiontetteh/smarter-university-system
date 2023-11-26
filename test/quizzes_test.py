bdd294cc09ba65d9c4470620f07ca03606c53d6d
Adheet Agrawal <ag.vidit1@gmail.com>
2023-11-26 14:33:22 -0500

    adding correct unit test cases.

--- test/quizzes_test.py
+++ test/quizzes_test.py
@@ -6,58 +6,58 @@ class QuizzesTest(unittest.TestCase):
 
     def setUp(self):
         # Run tests on non-production data
-        self.ctrl = QuizzesController('quizzes_test.json')
+        self.ctrl = QuizzesController('assignments_test.json')
 
     def test_expose_failure_01(self):
         """
-        This test case aims to expose a crash when adding a quiz with missing parameters.
-        Expected crash: quizzes_controller.py, line 64
+        This test case aims to expose a crash when adding a quiz with title passed as integer variable.
+        This fails as this will give unsupported operand type(s) for +: 'int' and 'str'
+        Expected crash: quizzes_controller.py, line 63
         """
 
         self.ctrl.clear_data()
-        # Not passing the available_date param
-        # This will give missing positional argument: 'available_date'
-        quiz_id = self.ctrl.add_quiz(title="Quiz title",text="text", due_date=datetime.now())
+        # Passing valid integer as quiz title
+        quiz_id = self.ctrl.add_quiz(12345,"text", datetime.now(), datetime.now())
         # Check that we have one quiz in the list
         quizzes = self.ctrl.get_quizzes()
-        # Since there is no quiz, the assertEquals function below fails
+        # Ideally, one quiz should be added and should be checked below
         self.assertEquals(len(quizzes), 1, "There is no quiz.")
         
 
     def test_expose_failure_02(self):
         """
-        This test case aims to expose a crash when attempting to add a question to a non-existent id.
-        Expected crash: quizzes_controller.py, line 76
+        This test case aims to expose a crash when attempting to add a question with datetime type passed as title.
+        This occurs as the save data is not able to serialize the title (datetime object) into a valid json.
+        Expected crash: quizzes_controller.py, line 81 which effectively calls save data in line 55
         """
         self.ctrl.clear_data()
-        quiz_id = self.ctrl.add_quiz(title="Quiz title",text="text", available_date=datetime.now(), due_date=datetime.now())
-        # Check that we have one quiz in the list
+        quiz_id = self.ctrl.add_quiz("Quiz title", "text", datetime.now(), datetime.now())
+        # Check that one quiz has been added in the list
         quizzes = self.ctrl.get_quizzes()
         self.assertEquals(len(quizzes), 1, "There is exactly one quiz.")
-        # Get that quiz by using the quiz_id
-        quiz = self.ctrl.get_quiz_by_id(quiz_id)
-        # Add a question usng an invalid quiz id
-        question_id = self.ctrl.add_question(quiz_id="2333", title="Question 1",text="Sample question")
-        self.assertIsNotNone(question_id,"This question cannot be added")
+        # Add a question usng the same quiz id but we pass datetime object as title in place of string
+        question_id = self.ctrl.add_question(quiz_id, datetime.now(), "Sample question")
+        # Asswer and check if the valid question is added.
+        self.assertIsNotNone(question_id, "A valid question_id is fetched.")
 
 
     def test_expose_failure_03(self):
         """
-        This test case aims to expose a crash when attempting to add an answer with incorrect parameter type.
-        Expected crash: quizzes_controller.py, line 92
+        This test case aims to expose a crash when attempting to add text containing Unicode escape sequence '\udc00'.
+        This occurs as generate_id fails when encoding as utf-8 and is part of utf-16
+        Expected crash: quizzes_controller.py, line 91
         """
         self.ctrl.clear_data()
-        quiz_id = self.ctrl.add_quiz(title="Quiz title",text="text", available_date=datetime.now(), due_date=datetime.now())
-        # Check that we have one quiz in the list
-        quizzes = self.ctrl.get_quizzes()
-        self.assertEquals(len(quizzes), 1, "There is exactly one quiz.")
-        # Get that quiz by using the quiz_id
-        quiz = self.ctrl.get_quiz_by_id(quiz_id)
+        quiz_id = self.ctrl.add_quiz("Quiz title","text", datetime.now(), datetime.now())
         # Add a question using quiz id
         question_id = self.ctrl.add_question(quiz_id, title="Question 1",text="Sample question")
-        # Test case fails when the value of parameter is_correct is passed as None instead of boolean.
-        answer_id=self.ctrl.add_answer(question_id, text="Sample answer", is_correct=None)
-        self.assertEquals(len(answer_id), 1, "There is answer for this question cannot be added.")
+        question = self.ctrl.get_question_by_id(question_id)
+        # Assert and check if the same question id can be fetched which has been added.
+        self.assertEquals(question_id, question.id, "Correct question is fetched.")
+        # Test case fails when the value of parameter text contains value conains unicode escape sequence '\udc00'.
+        answer_id = self.ctrl.add_answer(question_id, text='Value is \udc00', is_correct=None)
+        # Assert if valid answer id answer can be fetched. 
+        self.assertIsNotNone(answer_id, "A valid answer_id has returned.")
 
 if __name__ == '__main__':
     unittest.main()
